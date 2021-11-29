2e891e87dff317ed33d499a1007cae2f6cbe4bcd
Rafael Friedel <RafaFriedelDev@outlook.com>
2021-11-29 18:14:59 -0300

    exercicio 4, agora a pratica resolvido.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/agora-a-pratica4.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/agora-a-pratica4.js
@@ -1,4 +1,11 @@
 var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var media =0;
 for (var i=0;i < numbers.length; i+=1){
-console.log(numbers[i])
+media += numbers[i] / numbers.length
 }
+if (media > 20)
+    console.log("valor maior que 20")
+    else console.log("valor menor ou igual a 20");
+
+
+
