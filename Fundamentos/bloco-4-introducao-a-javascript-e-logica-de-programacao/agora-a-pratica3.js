0794046939232d5bff3e070ffccc34f5a6947aa9
Rafael Friedel <RafaFriedelDev@outlook.com>
2021-11-29 18:03:15 -0300

    resolvido 3 agora a pratica

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/agora-a-pratica3.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/agora-a-pratica3.js
@@ -1,4 +1,6 @@
 var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var media =0;
 for (var i=0;i < numbers.length; i+=1){
-console.log(numbers[i])
+media += numbers[i] / numbers.length
 }
+console.log(media)
