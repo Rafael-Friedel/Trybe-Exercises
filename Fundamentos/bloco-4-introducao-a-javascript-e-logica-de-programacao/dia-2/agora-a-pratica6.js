67d0055775178c03d7fe7df8cfb875aba6888697
Rafael-Friedel <RafaFriedelDev@outlook.com>
2021-11-30 14:29:48 -0300

    exercicio 6 pronto

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/agora-a-pratica6.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/agora-a-pratica6.js
@@ -1,4 +1,9 @@
-var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let quantidadeNumerosImpares = 0;
 for (var i=0;i < numbers.length; i+=1){
-console.log(numbers[i])
+    if (numbers [i] % 2 === 1 ) 
+    quantidadeNumerosImpares +=1; 
 }
+if (quantidadeNumerosImpares === 0)
+    console.log ("nenhum valor ímpar encontrado") 
+    else console.log (quantidadeNumerosImpares)
