c0c2dd77b6f89bfa0786be120b5584ee3c61ff90
Rafael-Friedel <RafaFriedelDev@outlook.com>
2021-11-30 14:47:22 -0300

    dia 4.3

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/agora-a-pratica2.js
+++
@@ -1,6 +0,0 @@
-var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-var soma =0;
-for (var i=0;i < numbers.length; i+=1){
-soma += numbers[i]
-}
-console.log(soma)
--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/agora-a-pratica6.js
+++
@@ -1,4 +0,0 @@
-var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (var i=0;i < numbers.length; i+=1){
-console.log(numbers[i])
-}
