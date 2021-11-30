4b3fd335c743bd51d5c9c2ce066acae48832ebeb
Rafael-Friedel <RafaFriedelDev@outlook.com>
2021-11-30 14:04:10 -0300

    exercicio 5 pronto

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/agora-a-pratica5.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/agora-a-pratica5.js
@@ -1,4 +1,8 @@
-var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (var i=0;i < numbers.length; i+=1){
-console.log(numbers[i])
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = numbers[0]
+for (var i=1;i < numbers.length; i+=1){
+let atual = numbers[i];
+if (atual > maior)
+maior = atual;
 }
+console.log (maior)
