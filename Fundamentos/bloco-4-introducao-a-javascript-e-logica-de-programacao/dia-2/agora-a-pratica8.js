9ff08ea96c7a53e2ea6b753d4cefb3a9d9af39e8
Rafael-Friedel <RafaFriedelDev@outlook.com>
2021-11-30 14:40:13 -0300

    exercicio 8 pronto

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/agora-a-pratica8.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/agora-a-pratica8.js
@@ -1,4 +1,5 @@
-var numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (var i=0;i < numbers.length; i+=1){
-console.log(numbers[i])
+var numbers = [];
+for (var i=1;i < 26; i+=1){
+numbers.push(i)
 }
+console.log (numbers)
