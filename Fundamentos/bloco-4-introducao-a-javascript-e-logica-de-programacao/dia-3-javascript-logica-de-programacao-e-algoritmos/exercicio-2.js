7ff8a9d43f60b91893213217f3b47b5fdab013f5
Rafael Friedel <RafaFriedelDev@outlook.com>
2021-11-30 18:29:13 -0300

    exercicio 2 pronto

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-2.js
@@ -0,0 +1,19 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// Copiar
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+var n = 5
+var asteriscos = '*';
+var input = '';
+
+
+for (var i = 0; i < n; i +=1 ){
+   input += asteriscos;
+    console.log(input)
+}
