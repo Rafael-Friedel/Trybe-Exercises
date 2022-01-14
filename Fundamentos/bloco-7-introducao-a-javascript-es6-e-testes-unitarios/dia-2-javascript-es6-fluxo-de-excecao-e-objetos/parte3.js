1b6afbaa731864e027c345d4d5b649ae5bd2b57f
Rafael <rafafriedel@outlook.com>
2022-01-13 21:38:22 -0300

    exercicio 8, parte 3

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte3.js
@@ -42,14 +42,13 @@ const lesson1 = {
 
   const valorChave = (obj,posição) => Object.values(obj)[posição]
 
+const verificaPar = (obj,key, value) =>{
+const arr = Object.entries(obj);
+let retorno = false;
+for (let index in arr) {
+  if (arr[index][0] === key && arr[index][1] === value) retorno = true;
+}
+return retorno;
+}
 
-// // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
-// // Copiar
-// console.log(getValueByNumber(lesson1, 0));
-// // Output: 'Matématica'
-// // Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
-// // Copiar
-// console.log(verifyPair(lesson3, 'turno', 'noite'));
-// // Output: true,
-// console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
-// Output: false
+console.log(verificaPar(lesson1, 'materia','Matemática'))
