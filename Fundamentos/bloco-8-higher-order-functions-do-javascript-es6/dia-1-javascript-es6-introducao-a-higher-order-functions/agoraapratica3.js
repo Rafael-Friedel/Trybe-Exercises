a2f33886279a98ba1dc20f8d7946c41aec43eccf
Rafael <rafafriedel@outlook.com>
2022-01-18 20:16:41 -0300

    exercicio 3 com ternarios =)

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/agoraapratica3.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/agoraapratica3.js
@@ -2,7 +2,7 @@
 // Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
 
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'C', 'A', 'N.A', 'A', 'D', 'C'];
 
 const checarespostas = (resposta, gabarito) =>{
     if (resposta === gabarito){
@@ -17,7 +17,9 @@ let totalDePontos = 0;
 for(let index in gabarito){
     let soma = func(arrayderesposta[index], gabarito[index])
     totalDePontos += soma
-}return `O aluno somou ${totalDePontos} pontos`
+}
+return (totalDePontos >= 7) ? `O aluno somou ${totalDePontos} pontos! Parabéns você foi aprovado!!` : `O aluno somou ${totalDePontos} pontos! Ainda não, dessa vez você foi reprovado!!`
+
 };
 
 console.log(myHOF(RIGHT_ANSWERS, STUDENT_ANSWERS, checarespostas));
