1b75bde791fb9bb989dae1ac60541dded3f4454e
Rafael <rafafriedel@outlook.com>
2022-01-19 19:21:21 -0300

    parafixar sort

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/parafixar.sort.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/parafixar.sort.js
@@ -9,9 +9,16 @@ const people = [
   { name: 'Bruna', age: 19 },
 ];
 
+// const peoples = ['mateus', 'ana', 'lucas', 'rafa', 'ezequiel']
 
+// console.log(peoples.sort())
 
-console.log(people.sort((idade1,idade2) => idade1.age - idade2.age));
-// 2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente .
+// console.log(people.sort());
 
-console.log(people.sort((idade1,idade2) => idade2.age - idade1.age));
+// console.log(people.sort((idade1,idade2) => idade1.age - idade2.age));
+// // 2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente .
+
+//  console.log(people.sort((idade1,idade2) => idade2.age - idade1.age));
+
+//  console.log(people.sort((nome1,nome2) => nome1.name > nome2.name))
+
