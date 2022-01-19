fec324975e0bc76475dfa85c4ac32938dce275db
Rafael <rafafriedel@outlook.com>
2022-01-19 15:32:36 -0300

    parafixar find 2

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/parafixar2.find.js
@@ -0,0 +1,10 @@
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (name) => name.length === 5
+  // Adicione seu código aqui:
+
+
+console.log(names.find(findNameWithFiveLetters));
