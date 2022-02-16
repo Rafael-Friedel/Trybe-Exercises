6b3465a3779cb83781e02868a870642d70d00709
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-02-16 20:03:57 -0300

    [inicio exercicio]

---
+++ Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+ render(){ return (
+    <div className="App">
+      <h1 className='hello-world'>HELLO WORLD!</h1>
+      <p>este é meu primeiro App React!!</p>
+    </div>
+  );
+}
+}
+
+export default App;
