624bda389ae6c68f1a2def7b643b96f2f5eace0f
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-02-22 16:09:06 -0300

    para fixar 11.2 - 3,4,5,6

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/componentescontrolados/src/Form.jsx
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/componentescontrolados/src/Form.jsx
@@ -8,33 +8,56 @@ class Form extends Component {
 
     this.state = {
       estadoFavorito: '',
+      idade: 0,
+      vaiComparecer: false,
+
     };
   }
 
 
-  handleChange(event) {
+  handleChange({target}) {
+    const {name} = target
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      estadoFavorito: event.target.value,
+      [name]: value,
     });
   }
 
   render() {
+    const {handleChange} = this;
+    const {estadoFavorito, idade, vaiComparecer} = this.state;
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
         <form className="form">
+          <fieldset> <legend>Qual Estado</legend>
           <label>
             Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+              <textarea name="estadoFavorito" value={estadoFavorito} onChange={handleChange} />
           </label>
+          </fieldset>
+          <fieldset><legend>Qual idade</legend>
+          <label>
+            Qual a sua idade?
           <input
             type="number"
             name="idade"
+            onChange={handleChange}
+            value={idade}
           />
+          </label>
+          </fieldset>
+          <fieldset> <legend>Vai comparecer</legend>
+          <label >
+            Você gostaria de conhecer?
           <input
             type="checkbox"
             name="vaiComparecer"
+            value={vaiComparecer}
+            onChange={handleChange}
           />
+          </label>
+          </fieldset>
         </form>
       </div>
     );
