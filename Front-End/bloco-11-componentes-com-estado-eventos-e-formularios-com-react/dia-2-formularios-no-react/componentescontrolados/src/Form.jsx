3bead6226fecdbe2ac217ea7cd0df6ecc272e2e7
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-02-23 12:23:43 -0300

    terminando exercicios para fixar

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/componentescontrolados/src/Form.jsx
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/componentescontrolados/src/Form.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Form extends Component {
   constructor() {
@@ -7,56 +7,66 @@ class Form extends Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
-      estadoFavorito: '',
+      estadoFavorito: "",
       idade: 0,
       vaiComparecer: false,
-
     };
   }
 
-
-  handleChange({target}) {
-    const {name} = target
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     this.setState({
       [name]: value,
     });
   }
 
   render() {
-    const {handleChange} = this;
-    const {estadoFavorito, idade, vaiComparecer} = this.state;
+    const { handleChange } = this;
+    const { estadoFavorito, idade, vaiComparecer } = this.state;
     return (
       <div>
-        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <h1>
+          Estados e React - Tecnologia fantástica ou reagindo a regionalismos?
+        </h1>
         <form className="form">
-          <fieldset> <legend>Qual Estado</legend>
-          <label>
-            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={estadoFavorito} onChange={handleChange} />
-          </label>
+          <fieldset>
+            {" "}
+            <legend>Qual Estado</legend>
+            <label>
+              Diga qual o seu Estado favorito! De React ou do Brasil, você
+              decide! =)
+              <textarea
+                name="estadoFavorito"
+                value={estadoFavorito}
+                onChange={handleChange}
+              />
+            </label>
           </fieldset>
-          <fieldset><legend>Qual idade</legend>
-          <label>
-            Qual a sua idade?
-          <input
-            type="number"
-            name="idade"
-            onChange={handleChange}
-            value={idade}
-          />
-          </label>
+          <fieldset>
+            <legend>Qual idade</legend>
+            <label>
+              Qual a sua idade?
+              <input
+                type="number"
+                name="idade"
+                onChange={handleChange}
+                value={idade}
+              />
+            </label>
           </fieldset>
-          <fieldset> <legend>Vai comparecer</legend>
-          <label >
-            Você gostaria de conhecer?
-          <input
-            type="checkbox"
-            name="vaiComparecer"
-            value={vaiComparecer}
-            onChange={handleChange}
-          />
-          </label>
+          <fieldset>
+            {" "}
+            <legend>Vai comparecer</legend>
+            <label>
+              Você gostaria de conhecer?
+              <input
+                type="checkbox"
+                name="vaiComparecer"
+                value={vaiComparecer}
+                onChange={handleChange}
+              />
+            </label>
           </fieldset>
         </form>
       </div>
