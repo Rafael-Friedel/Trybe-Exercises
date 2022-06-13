5e8d4d8473c1c3da79d06d2c43f0e3e3ca3c0741
Rafael Friedel <RafaFriedelDev@outlook.com>
2022-06-13 19:19:58 -0300

    atualizando

--- Back-End/Bloco-22-Node-js/calculadora-de-massa-corporal/index.js
+++ Back-End/Bloco-22-Node-js/calculadora-de-massa-corporal/index.js
@@ -1,9 +1,6 @@
 const readline = require('readline-sync')
 
 console.log('Vamos calcular seu índice de massa corporal?')
-
-// const sub = (n1, n2) => n1 /n2; 
-
 const peso = readline.questionInt('Qual o seu peso?:    ');
 const altura = readline.questionFloat('Qual a sua altura?:   ');
 const result =  peso / (altura * altura)
@@ -19,10 +16,3 @@ else if(result > 40) ranking = 'Obesidade grau III e IV'
 else ranking = 'Não classificado'
 
 console.log(`Seu índice de massa corporal é ${result.toFixed(2)}, classificado como: ${ranking}`)
-
-// Abaixo de 18,5	Abaixo do peso (magreza)
-// Entre 18,5 e 24,9	Peso normal
-// Entre 25,0 e 29,9	Acima do peso (sobrepeso)
-// Entre 30,0 e 34,9	Obesidade grau I
-// Entre 35,0 e 39,9	Obesidade grau II
-// 40,0 e acima	Obesidade graus III e IV
