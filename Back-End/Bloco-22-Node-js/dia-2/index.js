c698996adb3d23026cf14271ccb02616a753bf96
Rafael Friedel <RafaFriedelDev@outlook.com>
2022-06-15 13:30:22 -0300

    adicionando requisito 1, e 2, do dia 2

---
+++ Back-End/Bloco-22-Node-js/dia-2/index.js
@@ -0,0 +1,7 @@
+function doMath(a, b, c) {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+            return reject(new Error('Informe apenas números')); 
+          }
+    });
+  }
