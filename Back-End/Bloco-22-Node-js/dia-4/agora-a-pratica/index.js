e59f2269c4bd95b68073e8bdb1f1aae38694f2a8
Rafael Friedel <RafaFriedelDev@outlook.com>
2022-06-19 18:05:47 -0300

    requisito 1 agora-pratica

--- Back-End/Bloco-22-Node-js/dia-4/agora-a-pratica/index.js
+++ Back-End/Bloco-22-Node-js/dia-4/agora-a-pratica/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-
+app.get('/ping', (req, res) => {
+  res.json({message: 'pong'})
+})
 
 app.listen(4000, () => {
     console.log('Aplicação ouvindo na porta 4000');
